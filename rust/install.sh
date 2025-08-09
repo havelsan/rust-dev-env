7ce802edb65b18471d3c86d8310396566ab02f84
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2025-08-09 15:35:34 +0300

    compact rust-dev-env preperation scripts

--- rust/install.sh
+++ rust/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Goto https://forge.rust-lang.org/infra/other-installation-methods.html
 #Find the Standalone Installers section.
 #Find the rust version compatible with your machine and download ( for me it is x86_64-unknown-linux-gnu) 
@@ -15,26 +16,9 @@ tar xvf *.tar.xz
 
 ./rust-*/install.sh --prefix=.
 
-
 d=$(ls *.tar.xz | sed -e 's/.tar.xz//')
 
 #echo $d | cut -d- -f2 > VERSION
 
 rm -Rf $d *.tar.xz
-
-echo '#!/bin/bash
-export RUST_HOME=`cd \`dirname $( readlink -f $BASH_SOURCE ) \` && pwd`
-export PATH=$RUST_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$RUST_HOME/lib:$RUST_HOME/lib/rustlib/x86_64-unknown-linux-gnu/lib/:$LD_LIBRARY_PATH
-export LIBRARY_PATH=$RUST_HOME/lib:$RUST_HOME/lib/rustlib/x86_64-unknown-linux-gnu/lib/:$LIBRARY_PATH
-export LIBRARY_PATH=$RUST_HOME/lib:$RUST_HOME/lib/rustlib/x86_64-unknown-linux-gnu/lib/:$LIBRARY_PATH
-export CPATH=$RUST_HOME/lib:$RUST_HOME/lib/rustlib/x86_64-unknown-linux-gnu/lib/:$CPATH
-export CARGO_HTTP_CHECK_REVOKE=false
-WHICH_RUSTC=$(which rustc)
-RUSTC_VERSION=$(rustc --version)
-
-echo ">>> RUST_HOME=$RUST_HOME "
-echo ">>> RUSTC_VERSION=$RUSTC_VERSION "
-'  > release
-
 
