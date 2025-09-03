a94b1e442e9be11ccfd03f242bf21c2422ae5805
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2025-09-03 07:57:24 +0300

    MPé

---
+++ cargo_home/bin/initialize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CURRENT_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+
+which cargo >/dev/null
+
+if [ $? != 0 ]
+then
+   echo " Source rust-dev-env/release please, cargo command can not be found"
+   exit 1
+fi
+
+if [ $# -lt 1 ]
+then
+        echo "Usage : $0 <cargo_toml_file>"
+        exit 1
+fi
+
+$CARGO_HOME/bin/update.sh $CARGO_HOME/etc/initial.Cargo.toml
+
+
---
+++ cargo_home/bin/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CURRENT_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+
+which cargo >/dev/null
+
+if [ $? != 0 ]
+then
+   echo " Source rust-dev-env/release please, cargo command can not be found"
+   exit 1
+fi
+
+if [ $# -lt 1 ]
+then
+        echo "Usage : $0 <cargo_toml_file>"
+        exit 1
+fi
+
+rm -Rf $CARGO_HOME/work
+mkdir -p $CARGO_HOME/work
+cp $1 $CARGO_HOME/work/Cargo.toml
+cd $CARGO_HOME/work
+cargo init
+cargo fetch 
+
+
