683a88987a2d5af40e395945269f2898323190e4
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2025-12-03 00:16:35 +0300

    MP

--- rust/install.sh
+++ rust/install.sh
@@ -4,10 +4,13 @@
 #Find the rust version compatible with your machine and download ( for me it is x86_64-unknown-linux-gnu) 
 #Then create rust sdk dir and prepare release files.
 
+
+ARCH=$(uname -p)
+
 if [ ! -f *.tar.xz ]
 then
     wget --no-check-certificate https://forge.rust-lang.org/infra/other-installation-methods.html
-    DOWNLOAD_URL=$(grep x86_64 other-installation-methods.html | grep  'https://.*dist/rust-[0-9].*-x86_64-unknown-linux-gnu.tar.xz' | sed -e 's/tar.xz.*/tar.xz/'| sed -e 's/.*"//'| grep https)
+    DOWNLOAD_URL=$(grep $ARCH other-installation-methods.html | grep  "https://.*dist/rust-[0-9].*-$ARCH-unknown-linux-gnu.tar.xz" | sed -e 's/tar.xz.*/tar.xz/'| sed -e 's/.*"//'| grep https)
     wget --no-check-certificate $DOWNLOAD_URL
     rm other-installation-methods.html*
 fi
